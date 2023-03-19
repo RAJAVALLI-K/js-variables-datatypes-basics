909de2c5cdc03b632c7c6dda18b9b99d7478a521
RAJAVALLI-K <rajavalli1932bca@gmail.com>
2023-03-19 21:13:18 +0530

    add js varaibles and operators basic concepts

---
+++ main.js
@@ -0,0 +1,96 @@
+//variables
+
+//1.var
+var x = 5;
+console.log(x);
+
+
+console.log(x);
+var x = 5;
+console.log(x);
+
+//2.let
+// console.log(val);
+let val = 100;
+console.log(typeof val);
+console.log(val);
+
+
+//3.const
+
+const maths = 80;
+
+const science = 70;
+let result = maths + science;
+console.log(result);
+
+//4.nothing
+
+subjecta = 90;
+subjectb = 50;
+c = subjecta + subjectb;
+console.log(c);
+
+
+//Data Types
+
+//1.string
+
+let name1 = 'raji';
+name2 = "valli";
+name3 = `rajii`;
+
+console.log(typeof name1);
+console.log(name1);
+console.log(name2);
+console.log(name3);
+
+//2.Number
+
+const num = 20;
+num1 = 13.459;
+let result1 = num + num1;
+console.log(result1);
+console.log(typeof result1);
+
+//3.bigint
+
+let ing = 12345679087536n;
+ing1 = 2357489087653n;
+result2 = ing + ing1;
+console.log(typeof result2);
+console.log(result2);
+
+
+//4.boolean
+
+const a = true;
+const b = false;
+console.log(typeof a);
+console.log(a);
+console.log(typeof b);
+console.log(b);
+document.write(a);
+
+// 5.undefined
+
+let birds;
+console.log(typeof birds);
+console.log(birds);
+
+
+//6.null
+
+const project = null;
+console.log(typeof project);
+console.log(project);
+
+// var na = null;
+// console.log(na);
+// alert(project);
+
+//2let
+
+console.log(y);
+let y = 5;
+console.log(y);
